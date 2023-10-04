4b5a73ef775286a11d823c5e69b94d431928e090
fm894511 <144338404+fm894511@users.noreply.github.com>
2023-10-04 13:41:25 +0200

    Delete Inventory Tutorial directory

--- Inventory Tutorial/Items/Item.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-class_name Item
-
-@export var name : String = ""
-@export var texture : Texture
-@export var max_stack_size : int = 1 # variable que indica si es estackeable y la cantidad
-
-var amount = 1 # cantidad de objetos del mismo tipo, sirve para los stacks
-
